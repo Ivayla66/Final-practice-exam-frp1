223abeca91e0fa53fb4d1731e72500521a4db987
Ivayla66 <ilie0018@hz.nl>
2025-04-13 22:26:57 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	resources/views/welcome.blade.php

--- resources/views/deliveries/create.blade.php
+++ resources/views/deliveries/create.blade.php
@@ -1,46 +1,35 @@
 <x-main>
     <div class="container">
-        <div class="columns mt-6 mb-6">
-            <div class="column">
-                <p class="title is-2">Create New Delivery</p>
-            </div>
-        </div>
-    </div>
+        <h1 class="title is-2">Create New Delivery</h1>
 
-    <section class="section">
-        <div class="container">
-            <div class="columns is-centered">
-                <div class="column is-half">
-                    <form method="POST" action="{{ route('deliveries.store') }}">
-                        @csrf
+        <form method="POST" action="{{ route('deliveries.store') }}">
+            @csrf
 
-                        <!-- Code Field Example -->
-                        <div class="field">
-                            <label class="label">Code</label>
-                            <div class="control">
-                                <input class="input @error('code') is-danger @enderror"
-                                       type="text"
-                                       name="code"
-                                       value="{{ old('code') }}">
-                                @error('code')
-                                <p class="help is-danger">{{ $message }}</p>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <!-- Repeat for other fields (description, price, etc.) -->
-
-                        <div class="field is-grouped">
-                            <div class="control">
-                                <button type="submit" class="button is-primary">Save</button>
-                            </div>
-                            <div class="control">
-                                <a href="{{ route('deliveries.index') }}" class="button is-light">Cancel</a>
-                            </div>
-                        </div>
-                    </form>
+            <!-- Code Field -->
+            <div class="field">
+                <label class="label">Code</label>
+                <div class="control">
+                    <input class="input @error('code') is-danger @enderror"
+                           type="text"
+                           name="code"
+                           value="{{ old('code') }}"
+                           required>
+                    @error('code')
+                    <p class="help is-danger">{{ $message }}</p>
+                    @enderror
                 </div>
             </div>
-        </div>
-    </section>
+
+            <!-- Other Fields (description, price, status, etc.) -->
+
+            <div class="field is-grouped mt-5">
+                <div class="control">
+                    <button type="submit" class="button is-primary">Save Delivery</button>
+                </div>
+                <div class="control">
+                    <a href="{{ route('deliveries.index') }}" class="button is-light">Cancel</a>
+                </div>
+            </div>
+        </form>
+    </div>
 </x-main>
