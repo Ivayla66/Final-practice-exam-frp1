76d2a433ae89da34ae96347bfaeeadef9011a75a
Ivayla66 <ilie0018@hz.nl>
2025-04-13 22:50:03 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	resources/views/welcome.blade.php

--- database/seeders/DeliverySeeder.php
+++ database/seeders/DeliverySeeder.php
@@ -14,18 +14,7 @@ class DeliverySeeder extends Seeder
     {
         // Create 10 random deliveries
         Delivery::factory()
-            ->count(8)
-            ->create();
-
-        // Create 2 specific status deliveries
-        Delivery::factory()
-            ->count(1)
-            ->planned()
-            ->create();
-
-        Delivery::factory()
-            ->count(1)
-            ->active()
+            ->count(10)
             ->create();
     }
 }
